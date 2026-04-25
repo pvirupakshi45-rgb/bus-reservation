331f13e4e5b11ffdaf1ccfcfdaaa2600c025e4e2
virupakship50@gmail.com <virupakship50@gmail.com>
2026-04-25 18:57:32 +0530

    Initial commit - Bus Reservation

---
+++ src/main/java/com/Reservation/bus/BusApplication.java
@@ -0,0 +1,15 @@
+package com.Reservation.bus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusApplication.class, args);
+		
+		System.out.println("hdhhddh");
+	}
+
+}
